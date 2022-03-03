0e02c1bab4c17ab1860ac93a12aa77e646e172a5
Vinicius Mendes <viniciusmendesrj>
2022-03-03 15:44:13 -0300

    Versão 1

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dr. Bernardo Resende</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/2736d36132.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<header>
+<div class="container">
+    <div class="rol">
+        <div class="col-sm">
+            <img src="images/logo.jpg" alt="Dr. Bernardo Resende">
+            <a href="#" class="btn align-middle">
+            AGENDAR CONSULTA
+            </a>
+        </div>
+    </div>
+</div>
+</header>
+
+<div id="menu">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="textoNavbar">
+                    <ul class="navbar-nav mr-auto">
+
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                      </li>
+                      
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Especialidades</a>
+                      </li>
+                      
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Quem Somos</a>
+                      </li>
+
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Onde Fica</a>
+                      </li>
+
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                      </li>
+                    
+                    </ul>
+                    
+                  </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+        
+</div>
+      
+<div id="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7">
+                <h1>BUSCANDO O MELHOR TRATAMENTO PARA O SEU SORRISO?</h1>
+                <p>Entre em contato com o consultório Dr. Bernardo Resende 
+e descubra qual o melhor tratamento para você</p>
+            </div>
+
+            <div class="col-sm bg-light shadow">
+                <h3>AGENDAR CONSULTA</h3>
+                <p>Preencha com seus dados abaixo e fale com nosso atendimento via WhatsApp</p>
+                <hr>
+                <div class="form-group">
+                    <input type="text" class="form-control"placeholder="Nome:">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control"placeholder="E-mail:">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control"placeholder="WhatsApp:">
+                </div>
+                <button class="btn">ENVIAR</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="esp">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h1>ODONTOLOGIA ESTÉTICA E FUNCIONAL</h1>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+                <div class="col-md-3">
+                    <a href="#">
+                      <div class="card mb-4 shadow-sm">
+                        <img class="card-img-top" src="images/protese.jpg" alt="Card image cap">
+                        <div class="card-body">
+                          <h2 class="card-text">PRÓTESE DENTÁRIA</h2>
+                        </div>
+                      </div>
+                    </a>
+                </div>
+
+                 <div class="col-md-3">
+                <a href="#">
+                  <div class="card mb-4 shadow-sm">
+                    <img class="card-img-top" src="images/lentes.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-text">LENTES DE CONTATO</h2>
+                    </div>
+                  </div>
+                </a>
+                </div>
+
+                <div class="col-md-3">
+                <a href="#">
+                  <div class="card mb-4 shadow-sm">
+                    <img class="card-img-top" src="images/implantes.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-text">IMPLANTES</h2>
+                    </div>
+                  </div>
+                </a>
+                </div>
+
+                <div class="col-md-3">
+                <a href="#">
+                  <div class="card mb-4 shadow-sm">
+                    <img class="card-img-top" src="images/canal.jpg" alt="Card image cap">
+                    <div class="card-body">
+                      <h2 class="card-text">ENDODONTIA (CANAL)</h2>
+                    </div>
+                  </div>
+                </a>
+                </div>
+
+               
+        </div>
+    </div>
+</div>
+
+<div id="contato">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <a href="#">
+                    <h1><i class="fab fa-whatsapp"></i> ENTRE EM CONTATO COM NOSSOS ATENDENTES</h1>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="bernardo">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7">
+                <h1>Dr. Bernardo Resende</h1>
+                <h2>CRO RJ 00001</h2>
+                <hr>
+                <p>Especializado em odontologia clínica e estética
+É um profissional com experiência para realizar
+o seu tratamento da melhor maneira</p>
+
+            <a href="#" class="btn">AGENDE SUA CONSULTA</a>
+            
+            </div>
+            <div class="col-sm">
+                    <img src="images/bernardo.png" alt="Bernardo Resende">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="insta">
+    <div class="container">
+        <div class="row">
+            <?php include 'insta.php' ?>
+        </div>
+    </div>
+</div>
+
+<div id="mapa">
+    <h1>ONDE ESTAMOS</h1>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14713.317307144129!2d-43.298933!3d-22.7902579!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x464b033c47c49f71!2sConsult%C3%B3rio%20Odontol%C3%B3gico%20Dr.%20Bernardo%20Resende!5e0!3m2!1spt-BR!2sbr!4v1646316720768!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <p>&copy <?php date('Y') ?> Dr. Bernardo Resende - Todos os direitos reservados</p>
+                <p><i class="fa fa-phone"></i> (21) 3939-2977</p>
+                <p>
+                    R. Marechal Bento Manoel, 88 - Jardim Vinte e Cinco de Agosto, Duque de Caxias - RJ, 25075-270
+                </p>
+            </div>
+            <div class="col-sm links">
+                <ul>
+                    <li><i class="fa fa-tooth"></i> <a href="#">Home</a></li>
+                    <li><i class="fa fa-tooth"></i> <a href="#">Especialidades</a></li>
+                    <li><i class="fa fa-tooth"></i> <a href="#">Quem Somos</a></li>
+                    <li><i class="fa fa-tooth"></i> <a href="#">Dr. Bernardo Resende</a></li>
+                    <li><i class="fa fa-tooth"></i> <a href="#">Endereço</a></li>
+                    <li><i class="fa fa-tooth"></i> <a href="#">Fale Conosco</a></li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fab fa-instagram"></i><a href="#"> bernardoresende</a></h5>
+                <h5><i class="fab fa-facebook"></i><a href="#"> bernardoresende</a></h5>
+                
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div id="footerCopy">
+    <p>Site criado por <a href="#"><i class="far fa-lightbulb"></i> Vinícius Mendes</a></p>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
